35a238ec90a16730a18e98020070ca14c6f83704
Edward P. Legaspi <czetsuya@gmail.com>
2022-03-28 12:04:26 +0800

    fix: xray segment name

---
+++ job-services/src/main/java/com/czetsuyatech/jobs/web/config/ConditionalXrayEnabled.java
@@ -0,0 +1,19 @@
+package com.czetsuyatech.jobs.web.config;
+
+import java.lang.annotation.Documented;
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
+
+@Target({ElementType.TYPE, ElementType.METHOD})
+@Retention(RetentionPolicy.RUNTIME)
+@Documented
+@ConditionalOnProperty(
+    prefix = "cx.aws.xray",
+    name = {"enabled"}
+)
+public @interface ConditionalXrayEnabled {
+
+}
